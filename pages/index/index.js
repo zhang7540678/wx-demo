235b79225ca6c1ebab68f07b4ea2ff5514a2f0df
1119740027 <1119740027@qq.com>
2018-11-09 17:57:03 +0800

    增加页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,50 +5,91 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    bannerList: [],
+    productList: [],
+    navList:[],
+    activeId: 0
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+    //获取产品类型
+    this.getProType();
+    //获取baner
+    this.getBannerList();
+    //获取产品数据
+    this.getProductList(1);
+  },
+  getProType() {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/tz/shop/goods/category/all',
+      data: {},
+      success(res) {
+        if(res.data.code == '0'){
+          var list = res.data.data;
+          list.unshift({
+            name: '全部',
+            id: 0
+          })
+          that.setData({
+            navList: res.data.data
           })
         }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      }
     })
+  },
+  getBannerList() {
+    var that = this;
+    wx.request({
+      url: 'https://api.it120.cc/tz/banner/list?key=mallName',
+      data: {},
+      success(res) {
+        if(res.data.code == '0'){
+          that.setData({
+            bannerList: res.data.data
+          })
+          console.log(that.data.bannerList);
+        }
+        
+      }
+    })
+  },
+  getProductList(num, id, like) {
+    var that = this;
+    !id && (id=0);
+    !like && (like='');
+    wx.request({
+      url: "https://api.it120.cc/tz/shop/goods/list",
+      data: {
+        categoryId: id,
+        nameLike: like,
+        page: num,
+        pageSize: 20
+      },
+      success(res) {
+        if(res.data.code == '0'){
+          var list = res.data.data;
+          that.setData({
+            productList: list
+          })
+        }
+      }
+    })
+  },
+  tabClick(e) {
+    var that = this;
+    console.log(e)
+    that.setData({
+      activeId: e.currentTarget.id
+    })
+    that.getProductList(1, that.data.activeId);
+  },
+  onPullDownRefresh() {
+    console.log('到底了')
+  },
+  onShareAppMessage(res) {
+    return {
+      title: '首页',
+      path: '/page/page/index'
+    }
   }
 })
